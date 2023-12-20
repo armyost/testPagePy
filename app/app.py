904a86d4ddb3cd08853459fdb93c46be792f9647
김종포 <armyost@naver.com>
2023-12-20 16:30:18 +0900

    231220_v11

--- app/app.py
+++ app/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
    return 'This is Home!'
 
+@app.route('/service/healthy')
+def home():
+   return '200'
+
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
